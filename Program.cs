225e4460833b20364560c801566bd03957dc7cc3
Piotr Slatala <Piotr Slatala>
2017-01-19 19:42:48 -0800

    finalized

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace SerializationPerfTest
     {
         static void Main(string[] args)
         {
+            Console.SetOut(new StreamWriter(File.OpenWrite(@"C:\tmp\perfout-small.txt")));
             var suite = new SerializationTestSuite<SmallObjectWithStrings, SmallObjectWithStringsDataContract, SmallObjectWithStringsBond, SmallObjectWithStringsProtobuf, SmallObjectWithStringsSerializable, SmallObjectWithStringsThrift>();
             suite.GenerateObject();
             PrintLengths(suite);
@@ -27,12 +28,14 @@ namespace SerializationPerfTest
 
             // var serialziationSummary = BenchmarkRunner.Run<SerializationTestSuite>();
             // var deserializationSummary = BenchmarkRunner.Run<DeserializationTests>();
-            BenchmarkRunner.Run<SerializationTestSuite<BiggerObjectWithBytes, BiggerObjectWithBytesDataContract, BiggerObjectWithBytesBond, BiggerObjectWithBytesProtobuf, BiggerObjectWithBytesSerializable, BiggerObjectWithBytesThrift>>();
+            // BenchmarkRunner.Run<SerializationTestSuite<BiggerObjectWithBytes, BiggerObjectWithBytesDataContract, BiggerObjectWithBytesBond, BiggerObjectWithBytesProtobuf, BiggerObjectWithBytesSerializable, BiggerObjectWithBytesThrift>>();
+            BenchmarkRunner.Run<SerializationTestSuite<SmallObjectWithStrings, SmallObjectWithStringsDataContract, SmallObjectWithStringsBond, SmallObjectWithStringsProtobuf, SmallObjectWithStringsSerializable, SmallObjectWithStringsThrift>>();
         }
 
         private static void PrintLengths<TBase, TContract, TBond, TProtobuf, TPoco, TThrift>(SerializationTestSuite<TBase, TContract, TBond, TProtobuf, TPoco, TThrift> testSuite) where TProtobuf : IMessage where TThrift : Thrift.Protocol.TBase
         {
             var type = typeof(SerializationTestSuite<TBase, TContract, TBond, TProtobuf, TPoco, TThrift>);
+            Console.WriteLine("Name\tbytes raw\tcompressed (optimal)\tcompressed (fastest)");
             foreach (var method in type.GetMethods())
             {
                 if (method.GetCustomAttributes(true).Any(x => x is BenchmarkAttribute))
@@ -45,14 +48,12 @@ namespace SerializationPerfTest
                     if (result is byte[])
                     {
                         var resultBytes = (byte[])result;
-                        Console.WriteLine("{0}: {1} bytes", method.Name, resultBytes.Length);
-                        Console.WriteLine("{0} compressed (optimal): {1} bytes", method.Name, Compress(resultBytes, CompressionLevel.Optimal).Length);
-                        Console.WriteLine("{0} compressed (fastest): {1} bytes", method.Name, Compress(resultBytes, CompressionLevel.Fastest).Length);
+                        Console.WriteLine("{0}\t{1}\t{2}\t{3}", method.Name, resultBytes.Length, Compress(resultBytes, CompressionLevel.Optimal).Length, Compress(resultBytes, CompressionLevel.Fastest).Length);
                     }
                     else if (result is string)
                     {
-                        var bytes = Encoding.UTF8.GetBytes((string)result);
-                        Console.WriteLine("{0}: {1} bytes", method.Name, bytes.Length);
+                        var resultBytes = Encoding.UTF8.GetBytes((string)result);
+                        Console.WriteLine("{0}\t{1}\t{2}\t{3}", method.Name, resultBytes.Length, Compress(resultBytes, CompressionLevel.Optimal).Length, Compress(resultBytes, CompressionLevel.Fastest).Length);
                     }
                     else if (result is int)
                     {
